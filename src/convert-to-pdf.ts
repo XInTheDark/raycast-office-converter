adf286159d960ac63cef7d9b8e8be261bb78c43a
Muzhen Gaming <61100393+XInTheDark@users.noreply.github.com>
2025-03-21 00:05:35 +0800

    Big rewrite
    - Both commands are now `view` type, although only partially. If the input is considered complete then it will run in the background as far as possible. This hopefully keeps the workflow clean for most users.
    - If the input is incomplete (e.g. no files selected, or format not specified), then we show a form to pick files and specify the format.
    - Use Raycast API for getting selected files.

--- src/convert-to-pdf.ts
+++
@@ -1,5 +0,0 @@
-import convertFiles from "./convert-file";
-
-export default async function convertToPDF(props: Parameters<typeof convertFiles>[0]) {
-  await convertFiles({ ...props, arguments: { ...props.arguments, format: "pdf" } });
-}
